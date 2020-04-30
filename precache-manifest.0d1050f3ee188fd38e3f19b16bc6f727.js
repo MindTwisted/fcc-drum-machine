c6e4a24f95dcf0c6b74bf17817c7ed3ecc7400f3
Moiseienko Mykola <mmoiseienko@gfl.geeksforless.net>
2020-04-30 20:40:17 +0300

    Updates

---
+++ precache-manifest.0d1050f3ee188fd38e3f19b16bc6f727.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "976438dce6ba5ceb11ad602cc45d386d",
+    "url": "https://mindtwisted.github.io/fcc-drum-machine/index.html"
+  },
+  {
+    "revision": "1e867eb476b72eba5d0e",
+    "url": "https://mindtwisted.github.io/fcc-drum-machine/static/css/2.cd1899df.chunk.css"
+  },
+  {
+    "revision": "5f7f5153bb6ba14a9a09",
+    "url": "https://mindtwisted.github.io/fcc-drum-machine/static/css/main.217870f3.chunk.css"
+  },
+  {
+    "revision": "1e867eb476b72eba5d0e",
+    "url": "https://mindtwisted.github.io/fcc-drum-machine/static/js/2.06789438.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "https://mindtwisted.github.io/fcc-drum-machine/static/js/2.06789438.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f7f5153bb6ba14a9a09",
+    "url": "https://mindtwisted.github.io/fcc-drum-machine/static/js/main.543eb5f2.chunk.js"
+  },
+  {
+    "revision": "29ea6ba90b3649a91a8e",
+    "url": "https://mindtwisted.github.io/fcc-drum-machine/static/js/runtime-main.3fe37c32.js"
+  }
+]);
